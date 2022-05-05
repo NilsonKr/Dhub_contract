40ac99dbeef73affcfdb9b330ca6d119cce9cec0
NilsonKr <nuotrok444@gmail.com>
2022-05-05 01:43:49 -0500

    :test_tube: Adding file upload and listing test related
    - register moved to util function

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+import { Dhub } from "../typechain";
 import { UserInfo } from "../types";
 
 describe("Dhub", function () {
@@ -15,6 +16,18 @@ describe("Dhub", function () {
     return { owner, deploy };
   };
 
+  const register = async (
+    deploy: Dhub,
+    name: string = USER_NAME,
+    url: string = USER_PROFILE_URL
+  ): Promise<UserInfo> => {
+    await deploy.register(name, url);
+
+    const user: UserInfo = await deploy.login();
+
+    return user;
+  };
+
   describe("Contract deployment", function () {
     it("Check contract is deployed correctly", async function () {
       const { owner, deploy } = await setup();
@@ -27,9 +40,7 @@ describe("Dhub", function () {
   describe("User related stuff", function () {
     it("Creates an user correctly", async function () {
       const { deploy } = await setup();
-      await deploy.register(USER_NAME, USER_PROFILE_URL);
-
-      const user: UserInfo = await deploy.login();
+      const user = await register(deploy);
 
       expect(user).to.does.not.be.undefined;
       expect(user.name).to.be.equals(USER_NAME);
@@ -64,6 +75,38 @@ describe("Dhub", function () {
 
       expect(user.name).to.be.equals(newName);
       expect(user.profileUrl).to.be.equals(newUrl);
+    });
+  });
+
+  describe("Files related stuff", function () {
+    it("Check initial files", async function () {
+      const { deploy } = await setup();
+      await register(deploy);
+
+      const files = await deploy.getFilesByUser();
+
+      expect(files).to.be.empty;
+    });
+
+    it("Correctly file upload", async function () {
+      const { deploy } = await setup();
+      await register(deploy);
+
+      await deploy.uploadFile({
+        id: 1,
+        url: "someurl",
+        title: "Pic",
+        description: "desc",
+        uploadDate: new Date().toDateString(),
+        size: 1000,
+      });
+
+      const files = await deploy.getFilesByUser();
+      const recentFile = await deploy.getFileByPosition(0);
+
+      expect(files).to.have.lengthOf(1);
+      expect(recentFile.id).to.be.equals(1);
+      expect(recentFile.title).to.be.equals("Pic");
     });
   });
 });
