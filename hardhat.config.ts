73800fbba69f5c37f5273958c61f7c05e9427bdd
NilsonKr <nuotrok444@gmail.com>
2022-05-16 21:16:06 -0500

    :rocket: Deploy contract to rinkeby testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,8 @@ import "solidity-coverage";
 
 dotenv.config();
 
+const { PROJECT_ID, PRIVATE_KEY } = process.env;
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -25,10 +27,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${PROJECT_ID}`,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
